4b528b5cd7a47264ad4250ad9393eb83ebc4b77f
tanvi <tanvighai02@gmail.com>
2023-10-08 19:43:16 -0400

    checking my first function created

---
+++ var.sh
@@ -0,0 +1,13 @@
+#if we name data it is called a variable
+#syntax to declare a variable
+
+#var_name = data
+
+a=10
+x=100
+y=true
+name= DevOPS
+
+#how to access the variables
+#$variable name
+echo vale of a =$a
