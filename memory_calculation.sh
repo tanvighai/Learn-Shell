7f6c6989279c5b7308a9a39064c755a11e59af76
tanvi <tanvighai02@gmail.com>
2023-10-18 22:22:44 -0400

    checking my first function created

--- memory_calculation.sh
+++ memory_calculation.sh
@@ -7,3 +7,8 @@ free_percentage=$(echo "scale=2; $free / $total * 100" | bc -l)
 
 #here bc is the command line calculator but it is not bale to process the decimal calculations
 echo $free_percentage
+
+#let me write the conditions
+#0-50 used -> good (green)
+#51-70 used -> orange
+#>70 -> red
