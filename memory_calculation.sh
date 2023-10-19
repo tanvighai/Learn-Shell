31359f82f8c6f09c8c404e112774e1584a048fb6
tanvi <tanvighai02@gmail.com>
2023-10-18 20:39:27 -0400

    checking my first function created

--- memory_calculation.sh
+++ memory_calculation.sh
@@ -3,7 +3,7 @@
 total=$(free | grep Mem | awk '{print $2}')
 free=$(free | grep Mem | awk '{print $4}')
 
-free_percentage=$(echo "scale=1; $free / $total * 100" | bc -l)
+free_percentage=$(echo "scale=2; $free / $total * 100" | bc -l)
 
 #here bc is the command line calculator but it is not bale to process the decimal calculations
 echo $free_percentage
